645be504c79846f03f097b50a3e6922771de05aa
Alex Shulepov <guardianofcookies@ya.ru>
2018-04-24 01:40:48 +0500

    shit

--- lab_6.py
+++ lab_6.py
@@ -14,14 +14,16 @@ class owner():
 
 		def encrypt(self):
 			self.k=get_k(self.n)
-			#print('k',self.k)
 			return (self.text*pow(self.k,self.d,self.n))%self.n, self.k
 
 		def decrypt(self,S):
 			return (S*inverse_of(self.k,self.n))%self.n
+
+		def decrypt(self,key,S):
+			return (S*inverse_of(key,self.n))%self.n
 		
 		def get_dec_key(self,k):
-			return pow(k,inverse_of(self.d,self.n),self.n)
+			return inverse_of(pow(k,self.d,self.n),self.n)
 
 
 		
@@ -29,8 +31,8 @@ class trans():
 	def __init__(self):
 		pass
 	def generateKeys(self):
-		self.p=gost.generate()
-		self.q=gost.generate()
+		self.p=11
+		self.q=17
 
 		phi=(self.p-1)*(self.q-1)
 		e = random.randrange(1, phi)
@@ -42,20 +44,27 @@ class trans():
 		self.e=e
 		self.d=d
 		return {'n':self.p*self.q,'d':d}
-	def sign(self,msg):
-		return pow(msg,self.e,self.p*self.q)
+
+	def sign(self,keys):
+		for i in range(len(self.Zs)):
+			if ((self.Zs[i]*keys[i])%(self.p*self.q))%2!=0:
+				return ERR('Cheater!')	
+		return OK(pow(self.element_to_sign,self.e,self.p*self.q))
 
 	def get_keys_info(self):
 		return {'secret':(self.e,self.p*self.q),'open':(self.d,self.p*self.q)}
 
 	def validate_sign(self,x,sign):
-		if pow(x,self.e,(self.p*self.q))==pow(x,self.e,(self.p*self.q)):
+		if pow(x,self.e,(self.p*self.q))==sign:
 			return True
 		else:
 			return False
+
 	def get_Zs(self, Zs):
 		self.Zs=Zs
-		return random.randint(0,len(Zs))
+		index=random.randint(0,len(Zs)-1)
+		self.element_to_sign=self.Zs.pop(index)
+		return index
 def blind_sign():
 	x=879749327239
 	print('text',x)
@@ -73,19 +82,32 @@ def blind_sign():
 	print('sign is ',T.validate_sign(x,y))
 	print('---------------')
 
+
 def blinded_sign():
 	A=owner()
 	T=trans()
-	A.set_keys(T.generateKeys())
-	Zs=[]
-	for i in range(8):
-		A.set_text(2**i)
+	A.set_keys(T.generateKeys())#p,q
+	Zs=[]	#list of encrypted msgs
+	msgs=[2**i for i in range(2,10)]
+	#msgs[4]=3
+	for m in msgs:
+		A.set_text(m)
+
 		Zs.append(A.encrypt())
 
-	k=T.get_Zs(list(map((lambda x:x[1]),Zs)))
+	k=T.get_Zs(list(map((lambda x:x[0]),Zs)))	#T choose one element to sign
+
+	key=Zs.pop(k)[1]#k of choosen element
 	keys=[]
-	for i in Zs:
-		keys.append(A.get_dec_key())
-	sign=T.sign
+	for i in range(len(Zs)):
+		keys.append(A.get_dec_key(Zs[i][1]))# z'=k**(-d) z'*z=x
+
+	sign=T.sign(keys)
+	if sign['status']:
+		y=A.decrypt(key,sign['value'])
+		print('sign is ',T.validate_sign(2**k,y))
+	else:
+		print(sign['value'])
+	
 blinded_sign()
 #(3*pow(32,31,77)*inverse_of(pow(32,31,77),77))%77
